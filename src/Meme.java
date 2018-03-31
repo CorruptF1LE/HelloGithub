47f709d363ee20bba94649ec65241d6059cf0b48
CorruptF1LE <anthonyanadon@gmail.com>
2018-03-30 23:06:13 -0700

    Testing Please Ignore

--- src/Meme.java
+++ src/Meme.java
@@ -1,6 +1,7 @@
 public class Meme {
     public static void main(String[] args){
         // Testing GitHub for the first time! :D
+        // Committed from IntelliJ
         System.out.println("Hello Github");
     }
 }
